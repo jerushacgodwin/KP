9d0953a94e0ed5b1fc50497a69bf333a0191659b
godwin <godwincthomas@gmail.com>
2025-08-30 19:52:49 +0530

    PayFess

--- packages/server/src/services/student.services.js
+++ packages/server/src/services/student.services.js
@@ -1,10 +1,22 @@
 const Student   = require("../models/student.model");
 const FeeStructure = require("../models/feestructure.model");
 const iClass = require("../models/iclass.model");
+const {extractAndMergeNestedData} = require("../utility/extractData");
 const db = require("../models");
-//const { Student, FeeStructure, iClass, Sequelize } = require("../models");
 const { Op, col } = require("sequelize");
 const {getNextSequenceValue} = require('./student.sequence.service');
+const fieldsToExtract = [
+    {
+        path: ['iClass'], 
+        targetField: 'class', 
+        nestedFields: ['name', 'group'] 
+    },
+    {
+        path: ['feeStructures'], 
+        targetField: 'fee',
+        nestedFields: ['amount', 'due_date'] 
+    }
+];
 module.exports.createStudent = async (data,file)=>{
    const user_id = await getNextSequenceValue('user_id');
  const studentData = Student.studentForm(data, file);
@@ -27,17 +39,15 @@ module.exports.getClassIDs  = async (user_id) => {
 };
 module.exports.getAllStudents = async (studentDetail) => {
   try {
-    //console.log("Fetching all students with details:", studentDetail);
     const { school_id, page = 1, size = 10, search} = studentDetail;
    const pageNum = Math.max(1, parseInt(studentDetail.page, 10) || 1);
 const sizeNum = Math.max(1, parseInt(studentDetail.size, 10) || 10);
 const offset = (pageNum - 1) * sizeNum;
-//console.log('Available models:', Object.keys(models));
   const { Student, iClass } = db;
   const whereClause = { school_id };
 
 if (search) {
-  console.log("Search term:", search);
+
  whereClause[Op.or] = [
         { name: { [Op.like]: `%${search}%` } },
         { email: { [Op.like]: `%${search}%` } },
@@ -45,61 +55,72 @@ if (search) {
        { '$iClass.name$': { [Op.like]: `%${search}%` } }
  ]
 }
-// Debug: Check if iClass model has associate function
 
 //console.log('Before association - Student associations:', Object.keys(Student.associations || {}));
 //console.log('Before association - iClass associations:', Object.keys(iClass.associations || {}));
 const students = await Student.findAndCountAll({
     where: {...whereClause },
+    attributes: ['id','user_id', 'name', 'email', 'phone_no', 'class_id'] ,
   include: [
     {
       model: iClass,
       as: 'iClass',
-      attributes: ['class_id', 'name', 'group'] // Include the fields you need
+      attributes: ['class_id', 'name', 'group'] 
     },
      {
           model: db.FeeStructure,
-          as: "feeStructures",
-          required: false,
+          as: "feeStructures",         
           attributes: ["amount", "due_date"]
         }
-    // {
-    //   model: School,
-    //   as: 'school',
-    //   attributes: ['code', 'name']
-    // }
+  
   ],
 
   limit: sizeNum,
   subQuery: false, 
   offset,
-  distinct: true, // Ensure distinct results
+  distinct: true, 
   order: [["user_id", "ASC"]],
-  // other options...
+
 });
+//console.log('Students fetched:', students.rows[0].feeStructures);
+     const studentData = students.rows.map(student => {
+            return extractAndMergeNestedData(student.toJSON(), fieldsToExtract); // .toJSON() to get plain object
+        });
+//console.log('Extracted Student Data:', studentData);
     return {
-      data: students.rows,
+      data: studentData,
       total: students.count,
       page: pageNum,
       size: sizeNum,
     };
-// const studentsWithFees = await Student.findAll({
- 
-//     include: [{
-//       model: FeeStructure,
-//       where: { school_id },
-//       attributes: ['id', 'fee_type', 'amount', 'academic_year_id'],
-//     }]
-
-// });
-
-
-
-
-console.log("Students with Fee Structures:", JSON.stringify(studentsWithFees, null, 2));
 
   } catch (error) {
     console.error("Error fetching all students:", error);
+    throw error;
+  }
+};
+module.exports.getStudentById = async (studentId) => {
+    const { Student, iClass } = db;
+  try {
+    const student = await Student.findOne({
+      where: { user_id: studentId },
+      include: [
+        {
+          model: db.iClass,
+          as: "iClass",
+          attributes: ['class_id', 'name', 'group'] 
+        },
+         {
+          model: db.FeeStructure,
+          as: "feeStructures",
+          required: false,
+          attributes: ["amount", "due_date"]
+        }
+      ]
+    });
+    return student;
+  } catch (error) {
+    console.error("Error fetching student by ID:", error);
     throw error;
   }
 };
