8b77f8999620158f520f0e6bbe44d0b0a456845f
godwin <godwincthomas@gmail.com>
2026-02-09 14:29:48 +0530

    chore: remove temporary test and reset scripts

--- packages/billing/lib/test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.shopFunction = void 0;
-const shopFunction = (input) => {
-    return "Processed in shop";
-};
-exports.shopFunction = shopFunction;
