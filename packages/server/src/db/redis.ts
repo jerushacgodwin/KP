80abc4d00caa51c82f5797535eacdc492db205be
godwin <godwincthomas@gmail.com>
2026-02-09 14:40:02 +0530

    redis update

--- packages/server/src/db/redis.ts
+++ packages/server/src/db/redis.ts
@@ -4,8 +4,8 @@ const redisClient = createClient({
   url: process.env.REDIS_URL || 'redis://localhost:6379'
 });
 
-redisClient.on('error', (err) => );
-redisClient.on('connect', () => );
+redisClient.on('error', (err) => {});
+redisClient.on('connect', () => {});
 
 export const connectRedis = async () => {
   if (!redisClient.isOpen) {
