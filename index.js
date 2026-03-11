b7325989f97223f25a83fece2b494a93fa3a33c7
godwin <godwincthomas@gmail.com>
2026-03-11 22:07:59 +0530

    fix: v10 force-root standalone build (resolves Hostinger output directory error)

--- index.js
+++ index.js
@@ -1,2 +1,2 @@
-// Entry point for Hostinger Node.js selector pointing to Next.js standalone server
-require('./application/.next/standalone/server.js');
+// Entry point for Hostinger Node.js selector pointing to root standalone server
+require('./.next/standalone/server.js');
