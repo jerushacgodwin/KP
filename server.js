5c4a71e3a0b66f4fe2152f84a456e0bae861779b
godwin <godwincthomas@gmail.com>
2026-03-11 22:24:55 +0530

    fix: v11 definitive programmatic server (resolves Hostinger overwriting and 503 errors) (#50)

--- server.js
+++ server.js
@@ -1,2 +1,19 @@
-// Entry point for Hostinger Node.js selector
-require('./application/.next/standalone/server.js');
+const { createServer } = require('http')
+const { parse } = require('url')
+const next = require('next')
+
+const dev = false
+const app = next({ dev, dir: './application' })
+const handle = app.getRequestHandler()
+
+const port = process.env.PORT || 3000
+
+app.prepare().then(() => {
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true)
+    handle(req, res, parsedUrl)
+  }).listen(port, (err) => {
+    if (err) throw err
+    console.log(`> Ready on http://localhost:${port}`)
+  })
+})
