4f83bc73dabb6ef088970c22fde5cfd6268ebf55
godwin <godwincthomas@gmail.com>
2026-03-12 13:53:44 +0530

    fix: resolve monorepo module issues by pointing to src and using transpilePackages

--- application/next.config.mjs
+++ application/next.config.mjs
@@ -23,7 +23,9 @@ const nextConfig = {
    transpilePackages: ["@kp/billing", "@kp/server", "@kp/shop"],
   webpack: (config) => {
     config.resolve.alias["@src"] = path.resolve(__dirname, "src");
-    config.resolve.alias['@kp/billing'] = path.resolve(__dirname, '../packages/billing/lib');
+    config.resolve.alias['@kp/billing'] = path.resolve(__dirname, '../packages/billing/src');
+    config.resolve.alias['@kp/server'] = path.resolve(__dirname, '../packages/server/src');
+    config.resolve.alias['@kp/shop'] = path.resolve(__dirname, '../packages/shop/src');
     return config;
 
  
